c38c5cae03003b67616fb0c5e0c0ea9152bb3c05
Patcharapon Nuimark <toeybaa@hotmail.com>
2016-06-27 11:38:33 +0900

    distance calculation method implementation

--- cal_dist.py
+++ cal_dist.py
@@ -1,11 +1,19 @@
 import numpy as np
 
-def dist_cal():
-    v1 = np.load('/home/peth/Databases/rPascal/features/caffe/references/2008_003944.npy')
-    v2 = np.load('/home/peth/Databases/rPascal/features/caffe/references/2008_004121.npy')
-    dist = np.linalg.norm(v1-v2)
-    dist2 = np.linalg.norm(v2-v1)
-    print (str("d1: ") + str (dist))
+def dist_cal(path1, path2):
+    c1 = np.load(path1)
+    c2 = np.load(path2)
+    dist = np.linalg.norm(c1-c2)
+    dist2 = np.linalg.norm(c2-c1)
+   # print dist
+    print (str("Distance is ") + str (dist))
 #  print (str("d2: ")+ str(dist2))
 
-dist_cal()
+#v1 = input("Input first image directory: ")
+v1 = '/home/peth/Databases/rPascal/features/caffe/references/2008_004975.npy'
+print (str("path 1 is: ")+str(v1))
+v2 = '/home/peth/Databases/rPascal/features/caffe/references/2008_004984.npy'
+#v2 = input("Input second image directory: ")
+print (str("path 2 is: ")+str(v2))
+
+dist_cal(v1,v2)
